a2a549f495ada3f2e891ebb01907d30a3d25a32a
Nikolaifan24 <nikolaifan36@gmail.com>
2023-07-02 10:49:53 -0500

    cambios varios

--- validacion.js
+++ validacion.js
@@ -1,1 +1,33 @@
 //Haz tú validación en javascript acá
+function validar(){
+  var nombre = document.getElementById("name").value;
+  var email = document.getElementById("emails").value;
+  var asunto = document.getElementById("subject").value;
+  var message = document.getElementById("mensagem").value;
+
+
+  try {
+    if (nombre == "" || email == "" ||asunto =="" || message == "" ){
+        document.getElementById("button1").disabled = true;
+        throw new Error("Name cannot be blank");
+        
+    }
+    else{
+      document.getElementById("button1").disabled = false;
+      console.log(nombre);
+      console.log(email);
+      console.log(message);
+
+    }
+    return true;
+  } catch (error) {
+    alert(error.message);
+    return false;
+  }
+  
+
+
+}
+
+  
+  
